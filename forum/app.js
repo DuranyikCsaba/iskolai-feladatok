1e8bc11946907631c475eb1a7e20257c5742693b
DuranyikCsaba <duranyik.csaba.2019@osztalyterem.katolikuskeri.hu>
2025-02-08 14:37:24 +0100

    2
    app.js

--- forum/app.js
+++ forum/app.js
@@ -1,1 +1,34 @@
-import express from express;
+import express from "express"
+import db from "./src/config/db.js"
+import PosztModel from "./src/models/poszt.js"
+
+const app = express();
+const PORT = 3524;
+
+app.use(express.json());
+
+db.authenticate()
+.then(() => {
+    console.log("Az adatbázis kapcsolat kiépítése sikeres!")
+
+    db.modelManager.addModel(PosztModel);
+
+    db.sync({ force: true })
+    .then(()=>{
+        console.log("A modellek szinkronizálása sikeres!");
+
+        app.listen(PORT, () => {
+            console.log(`A webszerver elindult a  http://localhost${PORT}/ -URL-en`);
+        });
+    })
+    .catch((err)=> {
+        console.error("A modellek szinkronizációja sikertelen!");
+        console.error(err);
+    });
+
+    
+})
+.catch((err) => {
+    console.error("Adatbázis kapcsolat kiépítése sikertelen!");
+    console.error(err);
+});
