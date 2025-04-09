856769b9787183e533fdd7a03c26db60fae0a8f7
DuranyikCsaba <duranyik.csaba.2019@osztalyterem.katolikuskeri.hu>
2025-04-09 12:04:40 +0200

    2

--- java/menhely2024/menhelyCLI/src/main/java/hu/katolikuskeri/stakmaivizsga2024/cli/Main.java
+++ java/menhely2024/menhelyCLI/src/main/java/hu/katolikuskeri/stakmaivizsga2024/cli/Main.java
@@ -3,6 +3,7 @@ package hu.katolikuskeri.stakmaivizsga2024.cli;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,7 +11,7 @@ public class Main {
         try {
             kutyuses = Kutyus.forrasOlvasas(new File("allatok.csv"));
 
-        }catch (FileNotFoundException ex){
+        } catch (FileNotFoundException ex) {
             System.err.println("A forrásállomány nem található!");
             System.exit(1);
         }
@@ -18,35 +19,54 @@ public class Main {
         int osszeg = 0;
         int db = 0;
 
-        for(Kutyus elem : kutyuses){
-            if((elem.getKennelSsz() == 5) && (elem.getIvar() == 'H')){
+        for (Kutyus elem : kutyuses) {
+            if ((elem.getKennelSsz() == 5) && (elem.getIvar() == 'H')) {
                 osszeg += elem.getKor();
                 db++;
             }
         }
-        double atlag = (double) osszeg /db;
+        double atlag = (double) osszeg / db;
         System.out.printf("6. feladat: Az 5-ös kennelben található kan kutyák átlagkora %.1f év!\n", atlag);
 
         boolean volt = false;
         Kutyus megfelelo = null;
-        for(Kutyus elem : kutyuses){
-            if((elem.getKor() < 5) && (elem.getIvar() == 'N') && (elem.getFajta().equals("mopsz"))){
+        for (Kutyus elem : kutyuses) {
+            if ((elem.getKor() < 5) && (elem.getIvar() == 'N') && (elem.getFajta().equals("mopsz"))) {
                 volt = true;
                 megfelelo = elem;
                 break;
             }
         }
-        if (volt){
+        if (volt) {
             System.out.println("7. feladat: A menhelyen található megfelelő kutya:");
             System.out.printf("\tNév: %s\n\tIvar: %s\n\tFajta: %s\n\tKor: %d év\n\tKennel: %d.\n",
-            megfelelo.getNev(),
-            megfelelo.getIvar() == 'N' ? "Nőstény" : "Hím",
-            megfelelo.getFajta(),
-            megfelelo.getKor(),
-            megfelelo.getKennelSsz()
+                    megfelelo.getNev(),
+                    megfelelo.getIvar() == 'N' ? "Nőstény" : "Hím",
+                    megfelelo.getFajta(),
+                    megfelelo.getKor(),
+                    megfelelo.getKennelSsz()
             );
-        }else{
+        } else {
             System.out.println("7. feladat: A menhelyen nem található örökbefogadásra megfelelő kutya!");
         }
+
+
+        HashMap<Integer, Integer> statisztika = new HashMap<>();
+        for(Kutyus elem : kutyuses){
+            if(statisztika.containsKey(elem.getKennelSsz())){
+                int darab = statisztika.get(elem.getKennelSsz())+1;
+                statisztika.put(elem.getKennelSsz(), darab);
+            }
+            else{
+                statisztika.put(elem.getKennelSsz(), 1);
+            }
+        }
+
+        System.out.println("8. feladat: startisztika:");
+        statisztika.forEach((kennel, kutyakSzama) -> {
+            if (kutyakSzama > 20) {
+                System.out.printf("\t%d: %d db\n", kennel, kutyakSzama);
+            }
+        });
     }
 }
